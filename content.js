d13045169ed955ca6cf40e0c62407e6b7a8d21a2
Ender MIRIZ <ender.miriz@pru.edu.tr>
2023-06-26 21:28:19 +0300

    add changedData and OldData from characterData

--- content.js
+++ content.js
@@ -3,15 +3,30 @@ var audio = new Audio("https://cdn.freesound.org/previews/658/658264_6142149-lq.
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message,sender,sendResponse){
     alert("Getir Warehouse Helper is Working!")
+    
     new MutationObserver(function(mutations) {
-        audio.play();
-
+        for (let mutation of mutations) {
+            if (mutation.type === 'characterData') {
+                const changedData = parseInt(mutation.target.data);
+                const OldData = parseInt(mutation.oldValue);
+                console.log(`Changed data: ${changedData} from ${OldData}`);
+                if (changedData > OldData) {
+                    audio.play();
+                }
+            }
+        }
+        
         // Order Number :
-        const element = document.querySelector("#root > div > div.layout--M4fKj > div.ant-row.ant-row-top > div.ant-col.ant-col-xs-24.ant-col-md-18.ant-col-xl-20 > div > div.ant-row > div.ant-col.ant-col-xs-24.ant-col-md-20 > div > div:nth-child(4) > div > div > div:nth-child(1) > span")
+        const ordernum = document.querySelector("#root > div > div.layout--M4fKj > div.ant-row.ant-row-top > div.ant-col.ant-col-xs-24.ant-col-md-18.ant-col-xl-20 > div > div.ant-row > div.ant-col.ant-col-xs-24.ant-col-md-20 > div > div:nth-child(4) > div > div > div:nth-child(1) > span")
         // Print Order Number in Console :
-        console.log(element.textContent);
+        try {
+            console.log(ordernum.textContent);
+          }
+          catch(err) {
+          }
+        
     }).observe(
         document.querySelector('#root > div > div.layout--M4fKj > div.ant-row.ant-row-top > div.ant-col.ant-col-xs-24.ant-col-md-18.ant-col-xl-20 > div > div.ant-row > div.ant-col.ant-col-xs-24.ant-col-md-20 > div > div:nth-child(4) > span'),
-        { subtree: true, characterData: true, childList: true }
+        { subtree: true, characterData: true, childList: true , characterDataOldValue: true}
     );
 }
