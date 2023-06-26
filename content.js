ad7a21bab2cc3fe066cf826fb7d81ceddfdbc58b
Ender MIRIZ <ender.miriz@pru.edu.tr>
2023-06-27 00:46:25 +0300

    add timer for carrier

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 
 var audio = new Audio("https://cdn.freesound.org/previews/658/658264_6142149-lq.mp3");
+var timeraudio = new Audio("https://cdn.freesound.org/previews/387/387533_3829977-lq.mp3")
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message,sender,sendResponse){
     alert("Getir Warehouse Helper is Working!")
@@ -9,22 +10,43 @@ function gotMessage(message,sender,sendResponse){
             if (mutation.type === 'characterData') {
                 const changedData = parseInt(mutation.target.data);
                 const OldData = parseInt(mutation.oldValue);
-                console.log(`Changed data: ${changedData} from ${OldData}`);
+                var today = new Date();
+                var changeTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+                const currentTime = new Date();
+                const fivesecTime = new Date(currentTime.getTime() + 5000);
+                const alertTime = fivesecTime.getHours() + ":" + fivesecTime.getMinutes() + ":" + fivesecTime.getSeconds();
+
+                console.log(`Changed data: ${changedData} from ${OldData}, time: ${changeTime} alert time: ${alertTime}`);
                 if (changedData > OldData) {
                     audio.play();
+                    const delay = 5000;
+                    setTimeout(() => {
+                        const ordernumtimed = document.querySelector("#root > div > div.layout--M4fKj > div.ant-row.ant-row-top > div.ant-col.ant-col-xs-24.ant-col-md-18.ant-col-xl-20 > div > div.ant-row > div.ant-col.ant-col-xs-24.ant-col-md-20 > div > div:nth-child(4) > div > div > div:nth-child(1) > span")
+                        if(ordernum === ordernumtimed){
+                            var today = new Date();
+                            var changeTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                            console.log('alert time :',changeTime)
+                            timeraudio.play();
+                            console.log("zaman doldu");
+                        }
+                      }, delay);
                 }
             }
         }
-        
         // Order Number :
         const ordernum = document.querySelector("#root > div > div.layout--M4fKj > div.ant-row.ant-row-top > div.ant-col.ant-col-xs-24.ant-col-md-18.ant-col-xl-20 > div > div.ant-row > div.ant-col.ant-col-xs-24.ant-col-md-20 > div > div:nth-child(4) > div > div > div:nth-child(1) > span")
-        // Print Order Number in Console :
-        try {
-            console.log(ordernum.textContent);
-          }
-          catch(err) {
-          }
-        
+        // Carrier Name :
+        setTimeout(() => {
+            const carriername = document.querySelector("#root > div > div.layout--M4fKj > div.ant-row.ant-row-top > div.ant-col.ant-col-xs-24.ant-col-md-18.ant-col-xl-20 > div > div.ant-row > div.ant-col.ant-col-xs-24.ant-col-md-20 > div > div:nth-child(4) > div > div > div:nth-child(3) > span > span")
+            try {
+                // Print Order Number and Carrier Name in Console :
+                console.log(ordernum.textContent);
+                console.log(carriername.textContent);
+            } catch(err) {}
+          }, 500);
+
+
     }).observe(
         document.querySelector('#root > div > div.layout--M4fKj > div.ant-row.ant-row-top > div.ant-col.ant-col-xs-24.ant-col-md-18.ant-col-xl-20 > div > div.ant-row > div.ant-col.ant-col-xs-24.ant-col-md-20 > div > div:nth-child(4) > span'),
         { subtree: true, characterData: true, childList: true , characterDataOldValue: true}
